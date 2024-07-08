5e6794a968c9d86a12f58479dfc35d871409a9e7
ricdeo718 <juandeossa.nc@gmail.com>
2024-07-07 19:40:32 -0500

    se implementa hash de contraseña

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { CreateUser } from "../models/user.interface";
+import { hash } from "bcrypt";
 
 const Prisma = new PrismaClient();
 
@@ -15,15 +16,22 @@ export const usersServices = {
   //   return note;
   // },
   createUser: async (note: CreateUser) => {
+    const { email, password } = note;
+
+    const newPassword = await hash(password, 10);
+
     const createdUser = await Prisma.user.create({
       data: {
-        email: note.email,
-        password: note.password,
+        email,
+        password: newPassword,
       },
     });
 
     if (!createdUser) throw new Error("User not created");
 
-    return createdUser;
+    return {
+      id: createdUser.id,
+      email: createdUser.email,
+    };
   },
 };
